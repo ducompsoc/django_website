824f45c02b43ee332a4f6ea86cfc5f5bc55aad5b
Alistair Madden <alistair.john.madden@gmail.com>
2018-05-28 23:20:33 +0100

    Add quick links

---
+++ website/quick_links/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuickLinksConfig(AppConfig):
+    name = 'quick_links'
--- website/website/settings.py
+++ website/website/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'quick_links.apps.QuickLinksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
