782541aabb1389a8e0bf0c4a7a0dbf2e52423c97
Alistair Madden <alistair.john.madden@gmail.com>
2018-06-28 23:15:12 +0100

    Use dynamic absolute URLs

--- django/website/urls.py
+++ django/website/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('events/', include('events.urls')),
     path('admin/', admin.site.urls)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # Not good in production
+]
--- django/website/views.py
+++ django/website/views.py
@@ -3,6 +3,10 @@ import datetime
 
 
 class TemplateView(django.views.generic.TemplateView):
+    """
+    Defines a template view for all other views used in this site to extend.
+    Sets as extra content to every page the current year.
+    """
     extra_context = {'current_year': datetime.datetime.now().year}
 
     def get_context_data(self, **kwargs):
