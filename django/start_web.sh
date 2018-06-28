525d5385437d07e577045bef350fbb59ab257f01
Alistair Madden <alistair.john.madden@gmail.com>
2018-06-28 23:11:53 +0100

    Use a real wsgi web server (gunicorn)

--- django/start_web.sh
+++ django/start_web.sh
@@ -4,4 +4,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 create_admin.py
 python3 manage.py collectstatic --noinput
-python3 manage.py runserver 0.0.0.0:8000
+gunicorn website.wsgi -b 0.0.0.0:8000
